0e16b3fc70fd12265c0ee62d1ecdde010a7745a8
Rickson <rickson.simoes@hotmail.com>
2020-06-04 13:09:13 -0300

    :sparkles: Teste finalizado

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    const arquivo = uploadedFiles[0];
 
+    data.append('file', arquivo.file, arquivo.name);
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const fileSent = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(fileSent);
   }
 
   return (
